c58341c5fb15abd578bd89645b12dc056dc78515
lazzzc <lazyloadcharlotte@gmail.com>
2018-08-20 15:26:52 +0800

    create canary test

---
+++ sample/test/util_test.js
@@ -0,0 +1,8 @@
+// sample/test/util_test.js
+var expect = require('chai').expect;
+
+describe('util tests', function() {
+  it("should pass this canary test", function() {
+    expect(true).to.eql(true);
+  });
+});
